01b315233bcacf4704e2d6b3965b694555a8ce94
Timoty <timoty.granziero@gmail.com>
2020-07-04 22:15:17 +0200

    Remove other packages

--- src/main/java/it/xpug/ocp/rockpaperscissors/RockPaperScissorsGame.java
+++
@@ -1,9 +0,0 @@
-package it.xpug.ocp.rockpaperscissors;
-
-public class RockPaperScissorsGame {
-
-	public boolean beats(String a, String b) {
-		throw new RuntimeException("Not implemented!");
-	}
-
-}
